fe9a0f6cfceaef2126f88b78827767955a027f74
lgnq <dzzxzz@gmail.com>
2022-03-20 22:28:34 +0800

    rename the window title from windowTitle to YouTubeToPC

--- mainwindow.py
+++ mainwindow.py
@@ -60,7 +60,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "YouTubeToPC"))
         self.label.setText(_translate("MainWindow", "URL:"))
         self.label_2.setText(_translate("MainWindow", "Title:"))
         self.label_3.setText(_translate("MainWindow", "Resolution:"))
