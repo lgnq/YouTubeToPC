0f9078c14e60d884d58ac0a5b0d01f27cac13ed3
lgnq <dzzxzz@gmail.com>
2022-03-22 00:03:21 +0800

    Update main.py
    add "Download completes"

--- main.py
+++ main.py
@@ -44,8 +44,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         elif self.comboBox.currentText() == 'Best Available':
             self.yt.streams.first().download()
         else:
-            print(self.yt.streams.all()[self.comboBox.currentIndex()-2])
-            self.yt.streams.all()[self.comboBox.currentIndex()-2].download()
+            self.yt.streams[self.comboBox.currentIndex()-2].download()
+
+        self.console.append("Download completes!")
 
 if __name__ == '__main__':
     #每一pyqt5应用程序必须创建一个应用程序对象。sys.argv参数是一个列表，从命令行输入参数。
